4f93ca19261f2759abd544e3c3f9ace40ed73121
AlanDeSouza21 <as219de@uni9.edu.br>
2023-07-05 10:21:06 -0300

    interação usuario e animação interface

--- codigo-jogo.js
+++ codigo-jogo.js
@@ -1,4 +1,5 @@
-let fase = 13
+let fase = 2
+let calc_Fase = fase * 2
 let numeros = []
 let filtro = []
 let amarelo = document.getElementById("amarelo")
@@ -9,12 +10,13 @@ let restart = document.getElementById("restart")
 let puntuacao = document.getElementById("div-pontuacao")
 
 restart.style.display = 'none'
-
+//--------------------------------------- GERADOR DE NUMEROS --------------------------------\\
 function RandomNumbers(){
-  for (var i = 0; i < fase; i++) {
+  for (var i = 0; i < calc_Fase; i++) {
     var aleatorio = Math.floor(Math.random() * (3 - 1 + 1)) + 1
     numeros.push(aleatorio)
   }
+  // EVITA REPETIÇÕES
   let converteString = numeros.toString()
   let Tira_Virg = converteString.replace(/,/g, "")
   let repete_UM = Tira_Virg.replace(/111/g, 113)
@@ -25,7 +27,7 @@ function RandomNumbers(){
 }
 
 var intervalo =''
-
+//--------------------------------------- INICIA AÇÃO --------------------------------\\
 function inicio(){
   restart.style.display = 'block'
   start.style.display = 'none'
@@ -38,7 +40,7 @@ function inicio(){
 var numerosRand = RandomNumbers()
 var percorre = 0
 var intervalo2 = ''
-
+//--------------------------------------- SELECIONA BOTÕES QUE SERÃO ACESSOS --------------------------------\\
 function selecionaBTN(){
   console.log('vetor: '+numerosRand)
   var conteudoVETOR = numerosRand[percorre++]
@@ -57,11 +59,12 @@ function selecionaBTN(){
     }
     if(percorre > numerosRand.length){
       console.log('ACABOU!!!')
+
       clearInterval(intervalo)
     }
-
+    
 }
-
+//--------------------------------------- ANIMAÇÃO PARA ACENDER BOTÕES --------------------------------\\
 function acendeBTN_vermelho(){
   amarelo.style.background = 'rgb(0, 0, 0)'
   verde.style.background = 'rgb(0, 0, 0)'
@@ -86,6 +89,8 @@ function acendeBTN_amarelo(){
   intervalo2 = setTimeout(apagaTUDO, 700)
 }
 
+
+//--------------------------------------- AÇÃO PARA BOTÃO PISCAR --------------------------------\\
 function apagaTUDO(){
   amarelo.style.background = 'rgb(0, 0, 0)'
   verde.style.background = 'rgb(0, 0, 0)'
@@ -93,25 +98,109 @@ function apagaTUDO(){
 }
 
 let armaBTN_user = []
+let pontos = 0
+//--------------------------------------- INTERAÇÃO USUÁRIO E BOTÕES --------------------------------\\
+
+document.addEventListener("keyup", function(e){
+  if(e.key === "ArrowDown"){
+    interaVERDE(2)
+  }
+  else if(e.key === "ArrowLeft"){
+    interaAMARELO(3)
+  }
+  else if(e.key === "ArrowRight"){
+    interaVERMELHO(1)
+  }
+})
 
 function interaAMARELO(a){
   armaBTN_user.push(a)
   console.log('Vetor Interação: '+armaBTN_user)
+
+  amarelo.style.background = 'yellow'
+  verde.style.background = 'rgb(0, 0, 0)'
+  vermelho.style.background = 'rgb(0, 0, 0)'
+
+  intervalo2 = setTimeout(apagaTUDO, 100)
+
+  compara()
 }
 
 function interaVERDE(v){
   armaBTN_user.push(v)
   console.log('Vetor Interação: '+armaBTN_user)
+
+  amarelo.style.background = 'rgb(0, 0, 0)'
+  verde.style.background = 'green'
+  vermelho.style.background = 'rgb(0, 0, 0)'
+
+  intervalo2 = setTimeout(apagaTUDO, 100)
+  
+  compara()
 }
 
 function interaVERMELHO(verm){
   armaBTN_user.push(verm)
   console.log('Vetor Interação: '+armaBTN_user)
+
+  amarelo.style.background = 'rgb(0, 0, 0)'
+  verde.style.background = 'rgb(0, 0, 0)'
+  vermelho.style.background = 'red'
+  
+  intervalo2 = setTimeout(apagaTUDO, 100)
+
+  compara()
 }
 
+//--------------------------------------- LIGAÇÃO ENTRE FINAL DA ANIMAÇÃO E INTERAÇÃO DO USUÁRIO --------------------------------\\
 function interacao(){
-  var pontos = ''
+  interaVERMELHO(verm)
+  interaVERDE(v)
+  interaAMARELO(a)
+}
 
-  console.log('Vetor Interação: '+armaBTN_user)
+//--------------------------------------- COMPARAÇÃO --------------------------------\\
+let acumula_ERROS = []
+function compara(){
 
+  if (armaBTN_user.length == numerosRand.length) {
+    for (var n = 0; n < numerosRand.length; n++) {
+
+      if(armaBTN_user[n] == numerosRand[n]){
+        pontos = pontos + 25
+        acumula_ERROS.push('acerto')
+      }
+      else{
+        acumula_ERROS.push('erro')
+      }
+
+    }
+
+  }
+  let indice = 0
+  if (acumula_ERROS.length == numerosRand.length) {
+      for(mostra of acumula_ERROS){
+          if(mostra == 'erro'){
+            alert('FALHOU: pontuação máxima obtida: '+pontos)
+            throw new Error('ERRO!!!')
+          }
+          else{
+            indice++
+          }
+      }
+      if(acumula_ERROS.length == indice){
+        POS_comparacao()
+      }
+  }
+
+}
+
+//--------------------------------------- RESULTADO --------------------------------\\
+function POS_comparacao(){
+  fase = fase + 1
+  console.log('fase: '+fase)
+  alert('pontos: '+pontos)
+
+  RandomNumbers()
+  inicio()
 }
